6c5ca1e1732fdd08bf99791fdb0ab76fd4531398
André Pontes <33063611+Pottzork@users.noreply.github.com>
2019-12-14 19:47:11 +0100

    added entryfield and calculate button

--- PizzaCalc/MainActivity.cs
+++ PizzaCalc/MainActivity.cs
@@ -15,6 +15,17 @@ namespace PizzaCalc
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            var peopleEntry = FindViewById<EditText>(Resource.Id.peopleEntry);
+            var calculate = FindViewById<Button>(Resource.Id.calculateButton);
+            var pizzaCount = FindViewById<TextView>(Resource.Id.pizzaCountLabel);
+
+
+            calculate.Click += delegate
+            {
+                var count = int.Parse(peopleEntry.Text);
+                var pizzas = count / 3;
+                pizzaCount.Text = pizzas.ToString();
+            };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
